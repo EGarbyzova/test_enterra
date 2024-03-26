382992a584f572e4ec80b1acf8a70edbb8c8a30a
Elena Yaradkova <375295493848lena@mail.ru>
2024-03-26 17:55:11 +0300

    fixed

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,24 +1,5 @@
 import React, { useRef, useState, useEffect, useCallback } from "react";
 
-function animation(elem, fromX, fromY, toX, toY) {
-  const dx = fromX - toX;
-  const dy = fromY - toY;
-  let i = 1,
-    count = 20,
-    delay = 20;
-
-  function loop() {
-    if (i >= count) {
-      return;
-    }
-    i += 1;
-    elem.style.left = (fromX - (dx * i) / count).toFixed(0) + "px";
-    elem.style.top = (fromY - (dy * i) / count).toFixed(0) + "px";
-    setTimeout(loop, delay);
-  }
-  loop();
-}
-
 export default function MainComponent() {
   const movedSquare = useRef(null);
   const staticSquare = useRef(null);
@@ -36,15 +17,32 @@ export default function MainComponent() {
       setIsDisabled(false);
       setTimerActive(false);
       circle.current.classList.toggle("circle-animation");
-      circle.current.style.left = 0;
-      circle.current.style.top = 0;
+      circle.current.style = "";
     }
   }, [seconds, timerId]);
+
+  const animation = useCallback((elem, fromX, fromY, toX, toY) => {
+    const dx = fromX - toX;
+    const dy = fromY - toY;
+    let i = 1,
+      count = 20,
+      delay = 20;
+
+    function loop() {
+      if (i >= count) {
+        return;
+      }
+      i += 1;
+      elem.style.left = (fromX - (dx * i) / count).toFixed(0) + "px";
+      elem.style.top = (fromY - (dy * i) / count).toFixed(0) + "px";
+      setTimeout(loop, delay);
+    }
+    loop();
+  }, []);
 
   const handleClick = useCallback(() => {
     if (!timerId.current) {
       timerId.current = setInterval(() => {
-        console.log("timer");
         setSeconds((prev) => --prev);
       }, 1000);
       setIsDisabled(true);
@@ -53,27 +51,27 @@ export default function MainComponent() {
     circle.current.classList.toggle("circle-animation");
     animation(
       circle.current,
-      movedSquare.current.getBoundingClientRect().left,
-      movedSquare.current.getBoundingClientRect().top,
-      staticSquare.current.getBoundingClientRect().left,
-      staticSquare.current.getBoundingClientRect().top
+      movedSquare.current.getBoundingClientRect().left - 30,
+      movedSquare.current.getBoundingClientRect().top + 30,
+      staticSquare.current.getBoundingClientRect().left + 30,
+      staticSquare.current.getBoundingClientRect().top + 30
     );
-  }, [timerId]);
+  }, [timerId, animation]);
 
   return (
     <>
       <div className="square-container">
-        <div className="square vertical-animation" ref={movedSquare}>
+        <div className="square s1 vertical-animation" ref={movedSquare}>
           1
         </div>
         <div className="circle" ref={circle}></div>
-        <div className="square vertical-center" ref={staticSquare}>
+        <div className="square s2 vertical-center" ref={staticSquare}>
           2
         </div>
+        <button className="btn" onClick={handleClick} disabled={isDisabled}>
+          {timerActive ? seconds : "START"}
+        </button>
       </div>
-      <button className="btn" onClick={handleClick} disabled={isDisabled}>
-        {timerActive ? seconds : "START"}
-      </button>
     </>
   );
 }
